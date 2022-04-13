4aee9d71fbc343edce5f1bf0ce25cb54c3d9cc3f
kimmsoll <kimmsoll@naver.com>
2022-04-13 19:46:57 +0900

    feat: create toggle.js

---
+++ src/components/Toggle/Toggle.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import styles from './toggle.module.scss';
+
+const Toggle = () => {
+    const [isChecked, setIsChecked] = useState(false);
+    const handleToggle = () => {
+        setIsChecked((state) => !state);
+    }
+    
+    return (
+        <div className={styles.toggle}>
+            <label className={styles.toggleLabel}>
+                <input
+                checked={isChecked}
+                onChange={handleToggle}
+                className={styles.toggleCheckbox}
+                type="checkbox"
+                />
+                <div className={styles.ball} />
+                <div className={styles.toggleTexts}>
+                    <span style={{ color: isChecked ? 'gray' : 'black' }} className={styles.textLeft}>기본</span>
+                    <span style={{ color: isChecked ? 'black' : 'gray'}} className={styles.textRight}>상세</span>
+                </div>
+            </label>
+        </div>
+    );
+}
+
+export default Toggle;
