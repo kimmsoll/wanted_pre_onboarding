7c1465a90cd6dd389e70a61487d3a86d21e31d3a
kimmsoll <kimmsoll@naver.com>
2022-04-18 19:16:08 +0900

    style: reset.css 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
+import './App.css';
+import './reset.css';
 import App from './App';
 
 const container = document.getElementById('root');
